2979e4b6c0c802cc284bb75a45e68e6cccfbde1b
kangmiaoyuan <kangmiaoyuan@hydsoft.com>
2019-12-27 15:33:59 +0800

    全局唯一ID生成工具类-雪花算法

---
+++ sponge-cloud-security/src/main/java/tech/sponge/cloud/security/SpongeCloudSecurityApplication.java
@@ -0,0 +1,11 @@
+package tech.sponge.cloud.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpongeCloudSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpongeCloudSecurityApplication.class,args);
+    }
+}
---
+++ sponge-cloud-security/src/test/java/tech/sponge/cloud/security/SpongeCloudSecurityTest.java
@@ -0,0 +1,13 @@
+package tech.sponge.cloud.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpongeCloudSecurityTest {
+
+    @Test
+    public void test01(){
+
+    }
+}
